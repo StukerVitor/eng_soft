0a63057d1a15cf8e18f083e1ba8dd539e4dc9f7e
stukervitor <vitoralmeid12345@outlook.com>
2025-06-05 08:51:16 -0300

    adding swagger and fixing comments delete cascade

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,12 +5,16 @@ import authRoutes from './routes/authRoutes';
 import taskRoutes from './routes/taskRoutes';
 import commentRoutes from './routes/commentRoutes';
 import errorHandler from './middlewares/errorHandler';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './utils/swagger.json';
 
 // Create and configure Express app
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Routes
 app.use('/users', userRoutes);
